258ba81696de071c18e65afcf343b0e5cf177d36
suryauppalapati <suryauppalapati9@gmail.com>
2024-04-14 17:54:08 +0530

    Fix: problemRouter post request route updated according to REST standards

--- src/routes/v1/problems.routes.js
+++ src/routes/v1/problems.routes.js
@@ -7,7 +7,7 @@ problemRouter.get("/ping", problemController.problemPingController);
 
 problemRouter.get("/:id", problemController.getProblem);
 problemRouter.get("/", problemController.getProblems);
-problemRouter.post("/add", problemController.addProblem);
+problemRouter.post("/", problemController.addProblem);
 problemRouter.put("/:id", problemController.updateProblem);
 problemRouter.delete("/:id", problemController.deleteProblem);
 
