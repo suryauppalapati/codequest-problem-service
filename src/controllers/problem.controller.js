f1b419b93e2fdd753ce6f74a20f5ebebd0bd091f
Surya Uppalapati <suryauppalapati9@gmail.com>
2024-04-17 09:15:02 +0530

    Feat: Added http-status-code definitions for readability

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,25 +1,27 @@
+const { StatusCodes } = require("http-status-codes");
+
 function problemPingController(req, res) {
   return res.send("Problem controller is up and running!");
 }
 
 function addProblem(req, res) {
-  return res.send("addProblem");
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
 }
 
 function getProblem(req, res) {
-  return res.send("getProblem");
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
 }
 
 function getProblems(req, res) {
-  return res.send("getProblems");
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
 }
 
 function updateProblem(req, res) {
-  return res.send("updateProblem");
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
 }
 
 function deleteProblem(req, res) {
-  return res.send("deleteProblem");
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not implemented" });
 }
 
 module.exports = { problemPingController, addProblem, getProblem, getProblems, updateProblem, deleteProblem };
