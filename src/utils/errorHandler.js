3bee0c5008c2c892c6c37651ae0d26ed13bddc39
Surya Uppalapati <suryauppalapati9@gmail.com>
2024-04-23 02:06:12 +0530

    Refactor: Updated error response object structure

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,20 +1,21 @@
 const { StatusCodes } = require("http-status-codes");
 const BaseError = require("../errors");
 
+//Custom middleware to handle errors
 function handleError(err, req, res, next) {
   if (err instanceof BaseError) {
     return res.status(err.statusCode).json({
       success: false,
+      statusCode: err.statusCode,
       message: err.message,
-      details: err.details,
+      errors: err.details,
     });
   } else {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       success: false,
+      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
       message: "Internal server error",
-      details: {
-        statusCode: res.status,
-      },
+      errors: err,
     });
   }
 }
