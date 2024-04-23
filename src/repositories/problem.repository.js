b02be2eb09ce629bfee5f47368ef3597224afade
Surya Uppalapati <suryauppalapati9@gmail.com>
2024-04-24 02:30:09 +0530

    Feat: added loggers and removed try catch blocks

--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -1,58 +1,34 @@
+const logger = require("../config/logger.config");
 const { Problem } = require("../schema");
 
 class ProblemRepository {
   async createProblem(problemData) {
-    try {
-      const problem = await Problem.create({
-        title: problemData.title,
-        description: problemData.description,
-        testcases: problemData.testcases ? problemData.testcases : [],
-      });
-      return problem;
-    } catch (error) {
-      console.log("Error while creating new problem");
-      throw error;
-    }
+    const problem = await Problem.create({
+      title: problemData.title,
+      description: problemData.description,
+      testcases: problemData.testcases ? problemData.testcases : [],
+    });
+    return problem;
   }
 
   async getAllProblems() {
-    try {
-      const problems = await Problem.find({});
-      return problems;
-    } catch (error) {
-      console.log("Error while fetching problems");
-      throw error;
-    }
+    const problems = await Problem.find();
+    return problems;
   }
 
   async getProblemById(problemId) {
-    try {
-      const problem = await Problem.findById(problemId);
-      return problem;
-    } catch (error) {
-      console.log("Error while fetching problem with given id");
-      throw error;
-    }
+    const problem = await Problem.findById(problemId);
+    return problem;
   }
 
   async updateProblem(problemId, problemData) {
-    try {
-      const problem = await Problem.findOneAndUpdate({ _id: problemId }, problemData, { new: true });
-      return problem;
-    } catch (error) {
-      console.log("Error while updating problem with given id");
-      throw error;
-    }
+    const problem = await Problem.findOneAndUpdate({ _id: problemId }, problemData, { new: true });
+    return problem;
   }
 
   async deleteProblem(problemId) {
-    try {
-      const deleteResponse = await Problem.findByIdAndDelete(problemId);
-      return deleteResponse;
-    } catch (error) {
-      console.log("Error while deleting problem with given id");
-      throw error;
-    }
+    const deleteResponse = await Problem.findByIdAndDelete(problemId);
+    return deleteResponse;
   }
 }
 
